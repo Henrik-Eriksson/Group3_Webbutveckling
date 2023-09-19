7e8e6e981c2baab4dd665803733fb2130b340df3
AronKG <122611265+AronKG@users.noreply.github.com>
2023-09-19 18:18:04 +0200

    inlogning container is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
-import React, { useState } from 'react';
+//import React from 'react';
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css';
-import Hello from './compenents/hello.jsx'
+import Login from './compenents/login.jsx'
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
     <div className="App"> {/* Apply CSS class */}
-      <Hello />
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <Login />
     </div>
   );
 }
