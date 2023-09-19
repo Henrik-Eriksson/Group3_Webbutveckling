b5d0a70804dc87b57cd09af006ee457d8b27a65a
Henrik Eriksson <113356629+Henrik-Eriksson@users.noreply.github.com>
2023-09-19 16:44:17 +0200

    create simple template

--- src/Signup.jsx
+++ src/Signup.jsx
@@ -1,1 +1,54 @@
+import { createRoot } from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Button, Paper, Box, Grid, TextField, Checkbox, FormGroup, FormControlLabel} from '@mui/material'
+import styled from '@emotion/styled';
 
+
+const InfoField = styled(TextField) ({
+  width: 600,
+  height: 60,
+  color: 'red',
+});
+
+
+function Signup() {
+  return (
+    <Grid container>
+      <Grid item xs={8} sm ={6} md = {2}>
+        <Paper>
+        </Paper>
+        </Grid>
+        <Grid item xs={8} sm = {6} md = {8} spacing = {2}>
+        <Paper>
+          <body id = "signUpBox">
+          <h1>Sign Up For Application</h1>
+          <InfoField id ="signuptext" label = "Enter your First Name" variant="outlined" size = "large"/>
+          <p></p>
+          <InfoField id ="signuptext" label = "Enter your Last Name" variant="outlined" size = "large"/>
+          <p></p>
+          <InfoField id ="signuptext" label = "Enter your Username" variant="outlined" size = "large"/>
+          <p></p>
+          <InfoField id ="signuptext" label = "Enter your Email-address" variant="outlined" size = "large"/>
+          <p></p>
+          <InfoField id ="signuptext" label = "Enter your Password" variant="outlined" size = "large"/>
+          <p></p>
+          <InfoField id ="signuptext" label = "Confirm your Password" variant="outlined" size = "large"/>
+          <p></p>
+          <Button variant="contained"  sx={{width: 600, height: 60}} size = "large">CREATE YOUR ACCOUNT</Button>
+          <p>Already have an account? <m>click here!</m></p>
+          <p>Login with Google</p>
+          </body>
+        </Paper>
+        </Grid>
+        <Grid item xs={8} sm = {6} md = {2} spacing = {2}>
+        <Paper>
+        </Paper>
+        </Grid>
+    </Grid>
+  )
+}
+
+export default Signup;
+// For traditional rendering
+// ReactDOM.render(<App />, document.getElementById('root'));
