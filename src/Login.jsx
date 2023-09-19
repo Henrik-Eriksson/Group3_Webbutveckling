755a96318ef3a03a5f7208f34f8479df8b73d2c3
Johan Sollenius <32204219+11Johan11@users.noreply.github.com>
2023-09-19 18:51:04 +0200

    Update Login.jsx
    Added responsitivity, Roboto font, Fixed some styling issues etc..

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,34 +1,43 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Button, Paper, Box, Grid, TextField, Checkbox, FormGroup, FormControlLabel} from '@mui/material'
+import {Button, Paper, Box, Grid, TextField, Checkbox, FormGroup, FormControlLabel, Hidden} from '@mui/material'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Typography from '@mui/material/Typography';
 
 
 
 function Login() {
   return (
-    <Grid container>
-      <Grid item xs={8} sm ={6} md = {6}>
-        <Paper>
-          <div id = "signinbox1">
-          <h1>
-            Welcome to our application
-          </h1>
-          <h5>
+    <Grid container spacing={0} sx={{p: 5, alignItems: 'center', justifyContent: 'center' }}>
+      <Grid item xs={12} sm ={6} md = {6} sx={{ display: { xs: 'none', sm: 'none', md: 'block' }  }}>
+
+             
+
+        <Paper sx={{p:5}}>
+                <Typography variant="h2" gutterBottom>
+                         Welcome to our application
+                </Typography>
+
+                                <Typography variant="h5" gutterBottom>
             this is a example text for our application. this is a example text for our application. this is a example text for our application. 
             this is a example text for our application. this is a example text for our application. this is a example text for our application. 
             this is a example text for our application. this is a example text for our application. this is a example text for our application. 
             this is a example text for our application. this is a example text for our application. this is a example text for our application. 
-          </h5>
-          </div>
-        </Paper>
+                </Typography>
+
+        
+      </Paper>
         </Grid>
-        <Grid item xs={8} sm = {6} md = {6} spacing = {3}>
-        <Paper>
-          <div id="signinbox2">
-          <h2 id = "signin">
+        <Grid item xs={12} sm = {10} md = {6} sx={{p: {xl: 10, lg: 10, md: 0, xs: 0, sm: 0}, m: 0 }}>
+   
+        <Paper sx={{p:5}}>
+          <Typography variant="h4" gutterBottom>
             Sign In
-          </h2>
+          </Typography>
           <FormGroup>
           <TextField id ="outlined-basic" label = "Username" variant="outlined" size = "small"/>
           <p></p>
@@ -37,9 +46,13 @@ function Login() {
           <FormControlLabel required control = {<Checkbox defaultChecked size = "small" />} label = "Remember me"/>
           <Button id = "loginbutton">LOGIN</Button>
           </FormGroup>
-          <p>Don't have an account?</p>
-          <p>Login with your google account</p>
-          </div>
+          <Typography variant="paragraph" gutterBottom>
+            Don't have an account?
+          </Typography>
+          <Typography variant="paragraph" gutterBottom>
+            <br></br>Login with your google account
+          </Typography>
+       
         </Paper>
         </Grid>
     </Grid>
