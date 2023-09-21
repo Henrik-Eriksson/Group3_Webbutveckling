fcea68e0885dd6a90f9cebcd10d5833724781ec1
AronKG <122611265+AronKG@users.noreply.github.com>
2023-09-21 13:02:11 +0200

    Update Calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,28 +1,14 @@
-import dayjs from 'dayjs';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-import   "./calender.css"; 
+import React from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid' // a plugin!
 
-
-
-export default function Calendar() {
-  return (
-    <div className='container'>
-        <div className='event-container'>
-            <h1>events</h1>
-            <h1>Search butto</h1>
-            
-        </div>
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DemoContainer components={['DateCalendar']}>
-            <DateCalendar
-            referenceDate={dayjs('2022-04-17')}
-            views={['year', 'month', 'day']}
-            />
-        </DemoContainer>
-        </LocalizationProvider>
-    </div>
-  );
+export default class DemoApp extends React.Component {
+  render() {
+    return (
+      <FullCalendar
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridMonth"
+      />
+    )
+  }
 }
