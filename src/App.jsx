fb6daf3896d1916224399c89c2b0db19b12171f8
Johan Sollenius <32204219+11Johan11@users.noreply.github.com>
2023-09-21 14:59:12 +0200

    Update App.jsx
    Added route to calendar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage.jsx';
 import Login from './Login.jsx';
 import Signup from './Signup.jsx';
-
+import Calendar from './Components/Calendar.jsx'
 const App = () => {
  return (
    <Router>
@@ -10,6 +10,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/calendar" element={<Calendar />} />
        </Routes>
    </Router>
  );
