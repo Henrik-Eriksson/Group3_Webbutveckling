06bdb92c1bbb05f6f95dd98261aca382e698e1af
Henrik Eriksson <113356629+Henrik-Eriksson@users.noreply.github.com>
2023-09-21 13:39:46 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,10 @@
-//import React from 'react';
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css';
-//import Login from './components/login.jsx'
-import Calendar from "./components/Calender"
-import Login from './Login';
-//import Login from './Login.jsx'
-
-
+import React from 'react';
+import HomePageGrid from './components/HomePageGrid';
+import HomePage from './HomePage';
 
 function App() { 
-  
-
   return (
-    <div className="App"> {/* Apply CSS class */}
-      <Calendar />
-    <div className="App"> 
-    <Login/>
-    </div>
+    <HomePage/>
   );
 }
 export default App;  
