39162c89bbe611101e3f1b210c5f5842e81bff17
Henrik Eriksson <113356629+Henrik-Eriksson@users.noreply.github.com>
2023-09-21 15:50:17 +0200

    Update userData.js

--- src/models/userData.js
+++ src/models/userData.js
@@ -39,7 +39,7 @@ export const users = [
     id: 4,
     username: 'Mohamed',
     firstName: Mohamed',
-    lastName: 'Kesete',
+    lastName: 'Alkhaled',
     password: '26cab3a9af1f2235e2a66135dd2852f2d7c2cd38a7bd0114abb01835',  //SHA-2 Hashed password
     email: '101@hotmail.com',
     profilePicture: 'https://dogtime.com/wp-content/uploads/sites/12/2011/01/GettyImages-1294547052.jpg?resize=1200,630',
