2404182496f9346375ffddb8c8a025a1ae9427a9
AronKG <122611265+AronKG@users.noreply.github.com>
2023-09-21 10:11:10 +0200

    Calendar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css';
-import Login from './compenents/login.jsx'
+//import Login from './compenents/login.jsx'
+import Calendar from "./compenents/Calender"
 
 function App() {
   
 
   return (
     <div className="App"> {/* Apply CSS class */}
-      <Login />
+      <Calendar />
     </div>
   );
 }
