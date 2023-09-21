137add415eb91c7f77c60c96b81abcfe77ac963c
Henrik Eriksson <113356629+Henrik-Eriksson@users.noreply.github.com>
2023-09-21 12:24:38 +0200

    Update and rename Calender.jsx to Calendar.jsx

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -3,7 +3,7 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateCalendar } from '@mui/x-date-pickers/DateCalendar';
-import   "./Calender.css"; 
+import   "./calender.css"; 
 
 
 
