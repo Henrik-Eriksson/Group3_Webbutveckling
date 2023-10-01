b13a719810dd6ca89fe7317f66770fb2013b23b9
AronKG <122611265+AronKG@users.noreply.github.com>
2023-10-01 13:31:52 +0200

    modified the routing part temporarily

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import HomePage from './views/pages/HomePage.jsx';
 import Login from './views/pages/Login.jsx';
 import Signup from './views/pages/Signup.jsx';
@@ -6,12 +6,17 @@ import Calendar from './views/pages/Calendar.jsx'
 const App = () => {
  return (
    <Router>
+      <Link to= "/">HomePage</Link>
+      <Link to= "/login">Login</Link>
+      <Link to= "/signup">Signup</Link>
+      <Link to= "/calendar">Calendar</Link>
        <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/calendar" element={<Calendar />} />
        </Routes>
+    
    </Router>
  );
 };
