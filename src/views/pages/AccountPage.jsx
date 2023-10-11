6f3471d10f195d2073571348724b6c654f1fb92a
Henrik Eriksson <erhe21eb@student.ju.se>
2023-10-11 17:49:48 +0200

    Updated design of AccountPage.jsx

--- src/views/pages/AccountPage.jsx
+++ src/views/pages/AccountPage.jsx
@@ -1,96 +1,167 @@
-import  { useState } from "react";
-import { Grid, Typography, TextField, Button, Link } from "@mui/material";
+import { useState } from "react";
+import { Grid, Typography, TextField, Button, Link, Avatar, Box, Badge, IconButton, Modal } from "@mui/material";
+import BuildIcon from '@mui/icons-material/Build';
+import EditIcon from '@mui/icons-material/Edit';
 import ResponsiveAppBar from "../components/ResponsiveAppBar.jsx";
 
-
-const Register = () => {
-  const [formData, setFormData] = useState({
-    email: "",
-    password: "",
-    confirmPassword: "",
-  });
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    // Validate form data (e.g., check for password match)
-
-    // Send registration request to your authentication system or API
-    // For this example, we'll assume success
-    alert("Registration successful!");
-  };
-
-  return (
-    <> 
-    <ResponsiveAppBar/>
-    <Grid
-      container
-      justifyContent="center"
-      alignItems="center"
-      height="100vh"
-      style={{ background: "#AAF" }} // Change the background color here
-      >
-      <Grid item xs={10} sm={6} md={4}>
-        <form onSubmit={handleSubmit}>
-          <Typography variant="h4" align="center" gutterBottom>
-            Account
-          </Typography>
-          <TextField
-            type="email"
-            name="email"
-            label="Email"
-            fullWidth
-            margin="normal"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
-          <TextField
-            type="password"
-            name="password"
-            label="Password"
-            fullWidth
-            margin="normal"
-            value={formData.password}
-            onChange={handleChange}
-            required
-          />
-          <TextField
-            type="password"
-            name="confirmPassword"
-            label="Confirm Password"
-            fullWidth
-            margin="normal"
-            value={formData.confirmPassword}
-            onChange={handleChange}
-            required
-          />
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            fullWidth
-            margin="normal"
-          >
-            Account
-          </Button>
-        </form>
-        <div style={{ textAlign: "center", marginTop: "20px" }}>
-          <Link href="#">Already have an account?</Link>
-          <br />
-          <Link href="#">Change password</Link>
-          <br />
-          <Link href="#">Change email</Link>
-        </div>
-      </Grid>
-    </Grid>
-    </>
-  );
+const sampleUserData = {
+  id: 1,
+  firstname: "John",
+  lastname: "Doe",
+  eventcount: "42",
+  username: "exampleUser123",
+  email: "john.doe@example.com",
+  profilePicture: "https://via.placeholder.com/150",
 };
 
-export default Register;
+function Accountpage() {
+  const [avatarSrc, setAvatarSrc] = useState(sampleUserData.profilePicture);
+  const [openModal, setOpenModal] = useState(false);
+  const [currentPassword, setCurrentPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+
+  const handleChangePasswordClick = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const handleSavePassword = () => {
+    // Logic to save the new password
+    // Ensure the new password and confirm password are the same
+    // Validate the current password
+
+    handleCloseModal();
+  };
+
+  // Handle avatar update here
+  const handleAvatarChange = () => {
+    // Logic to change avatar picture.
+    // For the sake of this example, let's use another placeholder image
+    setAvatarSrc('https://via.placeholder.com/140');
+  };
+  return (
+    <>
+      <ResponsiveAppBar />
+      <div style={{
+        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.20), rgba(0, 0, 0, 0.20)),url(./src/assets/xd.png)`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        height: "100vh",
+        alignItems: "center",
+        justifyContent: "center"
+      }}>
+      <Box sx={{ display: 'flex', alignItems: 'center', p: 3 }}>
+        <Badge
+          overlap="circular"
+          anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+          badgeContent={
+            <IconButton size="small" color="primary" onClick={handleAvatarChange}>
+              <EditIcon />
+            </IconButton>
+          }
+        >
+          <Avatar src={avatarSrc} alt={sampleUserData.username} sx={{ width: 100, height: 100, marginRight: 2 }} />
+        </Badge>
+        <Typography variant="h4" sx={{ color: "whitesmoke", ml: 6}} gutterBottom>
+          Settings for {sampleUserData.username}
+        </Typography>
+      </Box>
+      <Box sx={{ pl: 20}}>
+        <TextField
+          variant="filled"
+          label="First Name"
+          defaultValue={sampleUserData.firstname}
+          sx={{ mb: 2, mr: 2, ml: 4, backgroundColor: "whitesmoke"}}
+        />
+        <TextField
+          variant="filled"
+          label="Last Name"
+          defaultValue={sampleUserData.lastname}
+          sx={{ mb: 2, backgroundColor: "whitesmoke", width: 218}}
+        />
+        <p></p>
+        <TextField
+          variant="filled"
+          label="Email"
+          defaultValue={sampleUserData.email}
+          sx={{ mb: 2, width: 438, ml: 4, backgroundColor: "whitesmoke"}}
+        />
+        <p></p>
+        <TextField
+          variant="filled"
+          label="Username"
+          defaultValue={sampleUserData.username}
+          sx={{ mb: 2, width: 438, ml: 4, backgroundColor: "whitesmoke"}}
+        />
+        <p></p>
+                <Button variant="contained" color="secondary" onClick={handleChangePasswordClick} sx={{ mt: 2, ml: 4,  width: 438, backgroundColor: "#F6D58D"}}>
+          Change Password
+        </Button>
+        <Modal
+          open={openModal}
+          onClose={handleCloseModal}
+          aria-labelledby="change-password-title"
+          aria-describedby="change-password-description"
+        >
+          <Box sx={{ 
+            position: 'absolute', 
+            top: '50%', 
+            left: '50%', 
+            transform: 'translate(-50%, -50%)', 
+            width: 400, 
+            bgcolor: 'background.paper', 
+            p: 4, 
+            boxShadow: 24 
+          }}>
+            <Typography variant="h6" id="change-password-title">
+              Change Password
+            </Typography>
+            <TextField 
+              type="password" 
+              fullWidth 
+              label="Current Password" 
+              value={currentPassword}
+              onChange={(e) => setCurrentPassword(e.target.value)} 
+              sx={{ mt: 2 }} 
+            />
+            <TextField 
+              type="password" 
+              fullWidth 
+              label="New Password" 
+              value={newPassword}
+              onChange={(e) => setNewPassword(e.target.value)} 
+              sx={{ mt: 2 }} 
+            />
+            <TextField 
+              type="password" 
+              fullWidth 
+              label="Confirm New Password" 
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)} 
+              sx={{ mt: 2 }} 
+            />
+            <Button 
+              variant="contained" 
+              color="primary" 
+              onClick={handleSavePassword} 
+              sx={{ mt: 2, backgroundColor: "#F6D58D", width: 400}}
+            >
+              Save New Password
+            </Button>
+          </Box>
+        </Modal>
+        <p></p>
+        <Button variant="contained" sx={{ mb: 2, ml: 4, width: 438, backgroundColor: "#F6D58D"}}>
+          Save changes
+        </Button>
+      </Box>
+    </div>
+    </>
+  );
+}
+export default Accountpage;
