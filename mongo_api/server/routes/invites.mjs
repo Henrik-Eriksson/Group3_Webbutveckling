41bd77745547ccf49f48282aa4b10f5c9e34a364
Johan Sollenius <joso0412@hotmail.com>
2023-10-11 18:59:59 +0200

    small buggfix

--- mongo_api/server/routes/invites.mjs
+++ mongo_api/server/routes/invites.mjs
@@ -63,7 +63,7 @@ router.get("/receivedInvites/:userId", async (req, res) => {
     let query = { invited: new ObjectId(req.params.userId) };
     let results = await collection.find(query).toArray();
     if (results.length === 0) {
-      res.status(404).send("No invites found for this user");
+      res.status(404).send({message: "No invites found for this user", result: results});
     } else {
       res.status(200).send(results);
     }
